ead936cd13ba485db01f62e4659d196d1b117e46
Armando Huerta <hutarmando10911@gmail.com>
2020-01-09 22:30:37 -0600

    Curso de Frontend Developer

---
+++ js-construccion-de-componentes/analicemos-nuestro-diseño.js
@@ -0,0 +1,35 @@
+/*
+¿Qué es un componente? 
+Analicemos nuestros diseños
+En esta clase la profesora Estefany Aguilar 
+nos explica qué es un componente en el mundo 
+del frontend. 
+También analizaremos el diseño del proyecto del curso.
+
+Un componente, tanto en diseño como desarrollo web, 
+es un elemento muy pequeño que tiene la capacidad 
+de ser reutilizado en diferentes partes de una aplicación. 
+Por ejemplo: botones, iconos, cards, entre otras. 
+Puedes apreciarlos en las plataformas que visitas todos los 
+días: Twitter, Facebook, Platzi, YouTube y muchas más
+
+Crear una carpeta que se llame componets
+dentro de ella tener sub carpetas que se llamen como los componentes
+
+*Components
+--Carrusel
+--Item
+    --Iconos
+    --Titulo
+    --Texto Principal
+    --Texto Secundario
+--Footer
+    --Texto Pricipal
+--header
+    --logo
+    --Menu desplegable
+--Buscador
+
+
+
+*/
