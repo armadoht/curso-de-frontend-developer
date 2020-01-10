6372ef87392510755508462b72954882bd076aaf
Armando Huerta <hutarmando10911@gmail.com>
2020-01-09 20:41:22 -0600

    Curso de Frontend Developer

---
+++ js-arquitectura-css/enlaces-de-arquitecturas-css.js
@@ -0,0 +1,18 @@
+/**
+* An Introduction To Object Oriented CSS
+ https://www.smashingmagazine.com/2011/12/an-introduction-to-object-oriented-css-oocss/
+
+* BEM
+ http://getbem.com/introduction/
+
+
+* SMACSS
+ http://smacss.com/
+
+
+ * ITCSS: Scalable and Maintainable CSS Architecture
+ https://www.xfive.co/blog/itcss-scalable-maintainable-css-architecture/
+
+*atomic design
+ https://bradfrost.com/blog/post/atomic-web-design/
+ */
